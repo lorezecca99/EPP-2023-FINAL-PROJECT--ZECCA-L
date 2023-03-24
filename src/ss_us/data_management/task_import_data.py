867f2960712fa2a76827f5f9832abba74a6e92b1
lorezecca99 <115668250+lorezecca99@users.noreply.github.com>
2023-03-24 17:45:46 +0100

    Update task_import_data.py

--- src/ss_us/data_management/task_import_data.py
+++ src/ss_us/data_management/task_import_data.py
@@ -11,7 +11,7 @@ from ss_us.data_management.import_data import import_dataset
 def task_import_dataset(produces):
     """Clean the data (Python version).
     This task allows to import the merged datasets from 2014 to 2018 
-    concering wages and hours worked in the US """
+    concering wages and hours worked in the US. """
     #data_info = read_yaml(depends_on["data_info"])
     url = 'https://www.dropbox.com/s/a0re73x92nl8ama/dataset_14_18.csv?dl=1'
     #url18='https://www.dropbox.com/s/3u4nzej8r1tsi78/dataset_18.csv?dl=1'
